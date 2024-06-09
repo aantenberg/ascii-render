7935b9279cbc6441aee8eed92f4f0fe648cc7707
Andrew Antenberg <aanten@sas.upenn.edu>
2024-06-09 13:24:29 -0400

    create vec3 module

---
+++ vec3.py
@@ -0,0 +1,58 @@
+import math
+
+class vec3:
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
+
+  def __repr__(self):
+    return f'({self.x}, {self.y}, {self.z})'
+
+  def length(self):
+    return math.sqrt(self.sqrlen())
+  
+  def sqrlen(self):
+    return self.x ** 2 + self.y ** 2 + self.z ** 2
+  
+  def __add__(self, other):
+    return vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+  
+  def __sub__(self, other):
+    return self + (other * -1)
+  
+  def __truediv__(self, num):
+    return vec3(self.x / num, self.y / num, self.z / num)
+  
+  def __mul__(self, num):
+    return vec3(self.x * num, self.y * num, self.z * num)
+  
+  def __iadd__(self, other):
+    self.x += other.x
+    self.y += other.y
+    self.z += other.z
+    return self
+  
+  def __isub__(self, other):
+    self.x -= other.x
+    self.y -= other.y
+    self.z -= other.z
+    return self
+
+  def __eq__(self, other):
+    return self.x == other.x and self.y == other.y and self.z == other.z
+  
+  def __neq__(self, other):
+    return not self == other
+  
+  def normalized(self):
+    return self / self.length()
+  
+  def dot(self, other):
+    return self.x * other.x + self.y * other.y + self.z * other.z
+  
+  def clone(self):
+    return vec3(self.x, self.y, self.z)
+  
+  def dist(self, other):
+    return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2)
